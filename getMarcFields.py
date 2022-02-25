610867d7c6d676efe7ccfdab6ec4752a53934223
erik radio <radio.erik@gmail.com>
2022-02-25 14:04:59 -0700

    Update getMarcFields.py

--- getMarcFields.py
+++ getMarcFields.py
@@ -1,13 +1,11 @@
-from sickle import Sickle
 import sys, json, csv
 from argparse import ArgumentParser
-import logging
-import requests
 from xml.etree import ElementTree as ET
-import codecs
 from datetime import datetime
 import copy, time, datetime
 from collections import defaultdict
+import glob
+from pathlib import Path
 
 # Personal names - author, editor, translator, and anything else that appears in the personal names fields
 # Title (transcribed from the piece, and normed)
@@ -22,35 +20,57 @@ def getData(record):
     bib = []
     data = {}
     roles = defaultdict(list)
+    roleList = []
 
     for marc in record.findall('{http://www.loc.gov/MARC21/slim}record'):
         # title
         title=marc.find('{http://www.loc.gov/MARC21/slim}datafield[@tag="245"]/{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
-        data['title'] = title.text
+        if title is not None:
+            data['title'] = title.text
+        else:
+            data['title'] = ''
         #  author
         for nameField in marc.findall('{http://www.loc.gov/MARC21/slim}datafield[@tag="100"]'):
             # print(name.attrib)
-            code = nameField.find('{http://www.loc.gov/MARC21/slim}subfield[@code="4"]')
-            name = nameField.find('{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
-            if code.text == 'aut':
-                data['author'] = name.text
+            if nameField is not None:
+                code = nameField.find('{http://www.loc.gov/MARC21/slim}subfield[@code="4"]')
+                name = nameField.find('{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
+                if code.text == 'aut':
+                    data['author'] = name.text
+            else:
+                data['author'] = ''
 
 
         # publisher
         pubName=marc.find('{http://www.loc.gov/MARC21/slim}datafield[@tag="264"]/{http://www.loc.gov/MARC21/slim}subfield[@code="b"]')
-        data['publisherName'] = pubName.text
+        if pubName is not None:
+            data['publisherName'] = pubName.text
+        else:
+            data['publisherName'] = ''
         # place of publication
         pubPlace=marc.find('{http://www.loc.gov/MARC21/slim}datafield[@tag="264"]/{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
-        data['publisherPlace'] = pubPlace.text
+        if pubPlace is not None:
+            data['publisherPlace'] = pubPlace.text
+        else:
+            data['publisherPlace'] = ''
         # date of publication
         pubDate=marc.find('{http://www.loc.gov/MARC21/slim}datafield[@tag="264"]/{http://www.loc.gov/MARC21/slim}subfield[@code="c"]')
-        data['publicationDate'] = pubDate.text
+        if pubDate is not None:
+            data['publicationDate'] = pubDate.text
+        else:
+            data['publicationDate'] = ''
         # language
         lang=marc.find('{http://www.loc.gov/MARC21/slim}datafield[@tag="041"]/{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
-        data['language'] = lang.text
+        if lang is not None:
+            data['language'] = lang.text
+        else:
+            data['language'] = ''
         # genre/form
         genre = marc.find('{http://www.loc.gov/MARC21/slim}datafield[@tag="655"]/{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
-        data['genre'] = genre.text
+        if genre is not None:
+            data['genre'] = genre.text
+        else:
+            data['genre'] = ''
         # various roles
 
 
@@ -62,10 +82,12 @@ def getData(record):
                 for name in names:
                     roles[code.text].append(name.text)
         data.update(roles)
+
         bib.append(data)
         # print(data)
 
-        # print(data)
+    # ['aut', 'dte', 'ctb', 'prt',hnr]
+        print(bib)
     return bib
 
 
@@ -77,7 +99,7 @@ def write(rows):
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
     if not rows:
-        raise ValueError("No data provided")
+        return rows
     with open('vd17_'+str(st)+'.csv','w') as outfile:
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -85,17 +107,27 @@ def write(rows):
             for field in fieldnames:
                 if field not in row:
                     row[field] = None
+            # print(row)
             writer.writerow(row)
 
 def main():
+    bibs = []
+    path = Path("/Users/erra1244/Desktop/vd17/marcworks")
+    for file in path.glob("*.xml"):
+        # print(file)
+        with open(file,'r') as xmlFile:
+            tree = ET.parse(xmlFile)
+            root = tree.getroot()
 
-    with open(sys.argv[1],'r') as xmlFile:
-        tree = ET.parse(xmlFile)
-        root = tree.getroot()
-        bibs = []
-        for record in root.findall('{http://docs.oasis-open.org/ns/search-ws/sruResponse}records/{http://docs.oasis-open.org/ns/search-ws/sruResponse}record/{http://docs.oasis-open.org/ns/search-ws/sruResponse}recordData'):
-            bibs.extend(getData(record))
-        write(bibs)
+            # print(root)
+            recordCount = root.find('./{http://docs.oasis-open.org/ns/search-ws/sruResponse}numberOfRecords')
+            recordCount = int(recordCount.text)
+            if recordCount > 0:
+                for record in root.findall('{http://docs.oasis-open.org/ns/search-ws/sruResponse}records/{http://docs.oasis-open.org/ns/search-ws/sruResponse}record/{http://docs.oasis-open.org/ns/search-ws/sruResponse}recordData'):
+
+                    bibs.extend(getData(record))
+    write(bibs)
+
 
 
 # make this a safe-ish cli script
